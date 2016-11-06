4db8b4658f44d167a2966db4775373863f6294ac
khalidaliweh <khalidaliweh@hotmail.com>
2016-11-06 17:27:35 -0500

    Initial commit

--- FutureTbd/FutureTbd/Controllers/HomeController.cs
+++ FutureTbd/FutureTbd/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace FutureTbd.Controllers
 {
@@ -10,9 +6,9 @@ namespace FutureTbd.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+          
 
-            return View();
+            return View("Index");
         }
     }
 }
