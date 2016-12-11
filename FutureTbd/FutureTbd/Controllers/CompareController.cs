c19ce17446829965b44e44e6378c85ef21f884cb
khalidaliweh <khalidaliweh@hotmail.com>
2016-12-11 17:42:21 -0500

    adding compare controller

---
+++ FutureTbd/FutureTbd/Controllers/CompareController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace FutureTbd.Controllers
+{
+    public class CompareController : Controller
+    {
+        public const string COMPARE_PAGE_TITLE = "Future TBD | Compare ";
+        // GET: Compare
+        public ActionResult Index()
+        {
+            ViewBag.Title = COMPARE_PAGE_TITLE;
+            return View("Compare");
+          
+
+
+        }
+    }
+}
